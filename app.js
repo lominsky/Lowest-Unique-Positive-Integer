9cffc9c668afc32e1a97a905699ffc5b60ace739
Louis Minsky <louis@louisminsky.com>
2017-05-14 01:21:20 -0400

    added firebase logging

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 8000;
+var firebase = require("firebase");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDkj3FbA-CFDcv20y2nT9zmZtNJ84r5XjQ",
+    authDomain: "lupi-490c9.firebaseapp.com",
+    databaseURL: "https://lupi-490c9.firebaseio.com",
+    projectId: "lupi-490c9",
+    storageBucket: "lupi-490c9.appspot.com",
+    messagingSenderId: "155410457297"
+  });
+
+var db = firebase.database();
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
@@ -29,7 +41,8 @@ function findSocketId(id) {
 
 function startGame() {
   for(i in users) {
-    users[i].guess = 0
+    users[i].guess = 0;
+    users[i].time = "No Guess";
   }
   var startTime = new Date();
   endTime = startTime.getTime() + 1000 * gameTime;
@@ -39,13 +52,22 @@ function endGame() {
   var guesses = [];
   for(i in users) {
     if(users[i].guess != 0) {
-      guesses.push(users[i]);
+      var temp = {
+        name: users[i].name,
+        guess: users[i].guess
+      }
+      guesses.push(temp);
     }
   }
 
   io.sockets.emit("stop", guesses);
 
-  if(users.length > 2) {
+  if(guesses.length > 2) {
+    var temp = {
+      players: users,
+      timestamp: firebase.database.ServerValue.TIMESTAMP
+    }
+    firebase.database().ref('games').push(temp);
     play();
   } else {
     endTime = -1;
@@ -69,11 +91,16 @@ io.on('connection', function (socket) {
   socket.on('add user', function (username) {
     if (addedUser) return;
 
+
+    var now = new Date();
+
     // we store the username in the socket session for this client
     var user = {
       id: socket.id,
       name: username,
-      guess: 0
+      guess: 0,
+      ip: socket.request.connection.remoteAddress,
+      time: "No Guess"
     }
     users.push(user);
     addedUser = true;
@@ -82,7 +109,6 @@ io.on('connection', function (socket) {
       play();
     }
 
-    var now = new Date();
     var timeRemaining = -1;
     if(endTime != -1) {
        timeRemaining = Math.floor((endTime - now.getTime())/1000);
@@ -103,7 +129,10 @@ io.on('connection', function (socket) {
   socket.on('new message', function(message) {
     var index = findSocketId(socket.id);
     if(parseInt(message) > 0) {
-      users[index].guess = parseInt(message)
+      users[index].guess = parseInt(message);
+      var now = new Date();
+      users[index].time = now.getTime();
+
     }
   });
 
