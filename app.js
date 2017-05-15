c68cbc647726b980cfd3c12e8c4026638f2a6862
Louis Minsky <louis@louisminsky.com>
2017-05-15 18:24:10 -0400

    loggggs?

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 80;
 var firebase = require("firebase");
 
 firebase.initializeApp({
@@ -87,6 +87,15 @@ var play = function() {
 io.on('connection', function (socket) {
   var addedUser = false;
 
+  var log = {
+    ip: socket.request.connection.remoteAddress,
+    headers: socket.request.headers,
+    connect: firebase.database.ServerValue.TIMESTAMP
+  }
+  firebase.database().ref('users/' + socket.id ).set(log);
+
+  firebase.database().ref('current').set(users);
+
   // when the client emits 'add user', this listens and executes
   socket.on('add user', function (username) {
     if (addedUser) return;
@@ -99,13 +108,18 @@ io.on('connection', function (socket) {
       id: socket.id,
       name: username,
       guess: 0,
-      ip: socket.request.connection.remoteAddress,
       time: "No Guess"
     }
     users.push(user);
+    //console.log(now + ": " + username + " (" + user.id + ") has connected.");
+    var index = findSocketId(socket.id);
+    firebase.database().ref('users/' + users[index].id + '/name').set(username);
+
+    firebase.database().ref('current').set(users);
+
     addedUser = true;
 
-    if(users.length > 2) {
+    if(users.length > 2 && endTime == -1) {
       play();
     }
 
@@ -136,23 +150,31 @@ io.on('connection', function (socket) {
     }
   });
 
+  socket.on('local ip', function(ip) {
+    var index = findSocketId(socket.id);
+    firebase.database().ref('users/' + users[index].id + '/local_ip').set(ip);
+    //users[index].local_ip = ip;
+  });
 
   // when the user disconnects.. perform this
   socket.on('disconnect', function () {
-    if (addedUser) {
       var index = findSocketId(socket.id);
-      users.splice(index, i);
 
-      // echo globally that this client has left
-      socket.broadcast.emit('users', {
-        numUsers: users.length
-      });
-    }
+      if(index) {
+        firebase.database().ref('users/' + users[index].id + '/disconnect').set(firebase.database.ServerValue.TIMESTAMP);
+        users.splice(index, 1);
+
+	socket.broadcast.emit('users', {
+	  numUsers: users.length
+	});
+      }
 
     if(users.length < 3) {
       endGame();
       clearTimeout(timer);
     }
+
+    firebase.database().ref('current').set(users);
   });
 
 
